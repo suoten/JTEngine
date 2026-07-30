7afcecb511709721e6c00806a896c8acb894f0ca
suoten <suoten@163.com>
2026-07-30 17:38:19 +0800

    fix: 移除前端测试文件和临时脚本

--- jte/web/_batch.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-process.argv.slice(2).forEach(f => {
-  const sep = f.indexOf(":::");
-  const path = f.substring(0, sep);
-  const b64 = f.substring(sep + 3);
-  fs.writeFileSync(path, Buffer.from(b64, "base64").toString("utf8"), "utf8");
-  console.log("OK: " + path);
-});
